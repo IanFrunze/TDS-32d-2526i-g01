d5b55220bfedc0edef3be0010750f337cd647e4b
A53118-ISEL <A53118@alunos.isel.pt>
2025-09-27 15:54:18 +0100

    Add tests for Board manipulation

--- src/test/kotlin/BoardTests.kt
+++ src/test/kotlin/BoardTests.kt
@@ -60,14 +60,67 @@ class BoardTests {
     @Test
     fun `get function with row outside range fails`() {
         assertFailsWith<IllegalArgumentException> {
-            TODO()
+            Board(side = 8).get(row = -1, col = 1)
+            Board(side = 8).get(row = 0, col = 1)
+            Board(side = 8).get(row = 27, col = 1)
         }
     }
 
     @Test
     fun `get function with col outside range fails`() {
         assertFailsWith<IllegalArgumentException> {
-            TODO()
+            Board(side = 8).get(row = 1, col = -1)
+            Board(side = 8).get(row = 1, col = 0)
+            Board(side = 8).get(row = 1, col = 27)
+            Board(side = 8).get(row = 1, col = '@')
+            Board(side = 8).get(row = 1, col = '[')
+        }
+    }
+
+    @Test
+    fun `changePiece function with row outside range fails`() {
+        assertFailsWith<IllegalArgumentException> {
+            Board(side = 8).changePiece(row = -1, col = 1)
+            Board(side = 8).changePiece(row = 0, col = 1)
+            Board(side = 8).changePiece(row = 27, col = 1)
+        }
+    }
+
+    @Test
+    fun `changePiece function with col outside range fails`() {
+        assertFailsWith<IllegalArgumentException> {
+            Board(side = 8).changePiece(row = 1, col = -1)
+            Board(side = 8).changePiece(row = 1, col = 0)
+            Board(side = 8).changePiece(row = 1, col = 27)
+            Board(side = 8).changePiece(row = 1, col = '@')
+            Board(side = 8).changePiece(row = 1, col = '[')
+        }
+    }
+
+    @Test
+    fun `addPiece function with row outside range fails`() {
+        assertFailsWith<IllegalArgumentException> {
+            Board(side = 8).addPiece(row = -1, col = 1, value = 'w')
+            Board(side = 8).addPiece(row = 0, col = 1, value = 'w')
+            Board(side = 8).addPiece(row = 27, col = 1, value = 'w')
+        }
+    }
+
+    @Test
+    fun `addPiece function with col outside range fails`() {
+        assertFailsWith<IllegalArgumentException> {
+            Board(side = 8).addPiece(row = 1, col = -1, value = 'w')
+            Board(side = 8).addPiece(row = 1, col = 0, value = 'w')
+            Board(side = 8).addPiece(row = 1, col = 27, value = 'w')
+            Board(side = 8).addPiece(row = 1, col = '@', value = 'w')
+            Board(side = 8).addPiece(row = 1, col = '[', value = 'w')
+        }
+    }
+
+    @Test
+    fun `addPiece function with a third color fails`() {
+        assertFailsWith<IllegalArgumentException> {
+            Board(side = 8).addPiece(row = 1, col = 1, value = 'y')
         }
     }
 
