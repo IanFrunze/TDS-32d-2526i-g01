a99ee3bae67e960d63605be5f6d89a452c7db490
Rafael Pereira <rafapear@outlook.pt>
2026-01-05 17:40:20 +0000

    refactor(DrawCard): remove BoardPreview component to simplify layout

--- reversi-app/src/main/kotlin/pt/isel/reversi/app/pages/lobby/lobbyViews/lobbyCarousel/drawCard/DrawCard.kt
+++ reversi-app/src/main/kotlin/pt/isel/reversi/app/pages/lobby/lobbyViews/lobbyCarousel/drawCard/DrawCard.kt
@@ -63,10 +63,6 @@ fun ReversiScope.GameCard(
         ) {
             HeaderBadge(statusText, statusColor, name)
 
-            BoardPreview(
-                board = state.board, modifier = Modifier.weight(4f).padding(vertical = 12.dp)
-            )
-
             ScorePanel(Modifier, state.board)
         }
     }
